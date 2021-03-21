da264e8ff3f9dcb48016d57858dd752bc14f4db0
danaAlcala <bigdalcala@gmail.com>
2021-03-21 01:02:02 -0700

    Upgraded landing pages for JLV and lucivay

---
+++ JLV/artwork.js
@@ -0,0 +1,92 @@
+const ARTWORK = [
+  {
+    "Name": "Gogo",
+    "Link": "https://www.juungle.net/#/assets/7367ce208fd36ad3156f84c94a7367a34e3f877a3a09533f39876d5758b2edd5",
+    "Image": "webGogo",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Harley Quinn",
+    "Link": "https://www.juungle.net/#/assets/738e9b29d324f2974eb192edd3e45a1cdcc06631ed76eca01f0a2691e8964ffe",
+    "Image": "webHarleyQuinn",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Taarna and Elinore",
+    "Link": "https://www.juungle.net/#/assets/90a199d6cadc8ae3d3acbb98d04f2ed041aa8e1e82db6ed23324d8019475aed9",
+    "Image": "webTaarnaElinore",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Björk",
+    "Link": "https://www.juungle.net/#/assets/161dcc7567170ed9166b526259c0a6925ae058a285ad13286640a92f032f733b",
+    "Image": "webBjörk",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Santanico Pandemonium",
+    "Link": "https://www.juungle.net/#/assets/5c6870fee07b0dabc5baf5aea6a2ff0091151132c4f9b1ac13ecd7054988465a",
+    "Image": "webSantanico",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Snape",
+    "Link": "https://www.juungle.net/#/assets/6acac1edaf0003cf9d898ade2c54e1a5c1bf502842f7e7f252fb0e5fce3ac8d1",
+    "Image": "webSnape",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Aragorn and Arwen",
+    "Link": "https://www.juungle.net/#/assets/032639bb022487f221e720848bdcb66f244aa3db02575cf90511f09f4bde7ac7",
+    "Image": "webAragornArwen",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Baby Yoda",
+    "Link": "https://www.juungle.net/#/assets/8c9678501bf8cad3f830f203e54c244eca986bc05a7162522c1c1fffaaf32943",
+    "Image": "webBabyYoda",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Bane",
+    "Link": "https://www.juungle.net/#/assets/e5a0dd53e494a39429f935c2829b77077d02302b90391e47554a38c4b0118fa9",
+    "Image": "webBane",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Batman West",
+    "Link": "https://www.juungle.net/#/assets/a5d1661cee66f6452fb1a5d327b065c6538688fce8b82b7f2434e573651aa8b9",
+    "Image": "webBatman_West",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Beast",
+    "Link": "https://www.juungle.net/#/assets/559b8c2c703bdc9fe0e872e2400adc26270487b4b91debf90184ed51c66f144f",
+    "Image": "webBeast",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Believer",
+    "Link": "https://www.juungle.net/#/assets/adbbb009424b968ea150ecb6c2d79ad4d9cd47f2b6c253cdf1566295790550c5",
+    "Image": "webBeliever",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Big Ern",
+    "Link": "https://www.juungle.net/#/assets/7d698231de34bb011529aecfbcb1b990dad65516f9fce5a0123742fbd1e3bddd",
+    "Image": "webBig_Ern",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Big Worm",
+    "Link": "https://www.juungle.net/#/assets/17be76b8ba023af54538c26e95d6d0816859157d663d33fc047a8ad1b80e751a",
+    "Image": "webBig_Worm",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Black Panther",
+    "Link": "https://www.juungle.net/#/assets/5b610dcdbe8284102f548a6c017a6586b4286397e872fe4822810354b640f690",
+    "Image": "webBlackPanther",
+    "Action": "Purchase with Bitcoin Cash"
+  }
+]
---
+++ lucivay/artwork.js
@@ -0,0 +1,56 @@
+const ARTWORK = [
+    {
+      "Name": "Hecates Slumber",
+      "Link": "https://www.juungle.net/#/assets/5327bbf88aa2f867dcd2d292a559d887e473af2898b13ee489f8fa34c6921599",
+      "Image": "webhecates_slumber",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "Beneath the Blue (1 of 2)",
+      "Link": "https://www.juungle.net/#/assets/f205bc61c35d0f1605a1cb59a43ab63764ead34a4f525df4bd8d59b1b79dea01",
+      "Image": "webBeneath_the_blue_1",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "Beneath the Blue (2 of 2)",
+      "Link": "https://www.juungle.net/#/assets/286783a65ed16272333d4dcf00d205eba8f0e07b7f36a2576deae774c9e5c339",
+      "Image": "webBeneath_the_blue_2",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "Death of Rats",
+      "Link": "https://www.juungle.net/#/assets/3d158750ec513b8df0609388be7c6a9f60c8d20ea54d0c83f4888b23980b595c",
+      "Image": "webDeathOfRats",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "The Crypto Collection - BCH",
+      "Link": "https://www.juungle.net/#/assets/50fbe6a894439c3d70a868e08282773a8a2a23180d6b73072fa4ac09ad6e1a61",
+      "Image": "webBCH",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "The Crypto Collection - DOGE",
+      "Link": "https://www.juungle.net/#/assets/0af16a1dda768565d6622671435cc7612ca571106d02b53c60e507044728d02d",
+      "Image": "webDOGE",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "The Crypto Collection - NANO",
+      "Link": "https://www.juungle.net/#/assets/039e22f49ba51dec3f56afa620e573b1a02e95cc8a5ace3a6c82ab38b3280cba",
+      "Image": "webNANO",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "The Crypto Collection - RVN",
+      "Link": "https://www.juungle.net/#/assets/992d1890bbaa72fec83bafeb6fd5751ec92121891d2c93b42907730f2ddd629e",
+      "Image": "webRVN",
+      "Action": "Purchase with Bitcoin Cash"
+    },
+    {
+      "Name": "The Crypto Collection - UNI",
+      "Link": "https://www.juungle.net/#/assets/3c04d8a136384005c864590032ddf379a173b06008431db43862c91aa9a3b1da",
+      "Image": "webuniswap",
+      "Action": "Purchase with Bitcoin Cash"
+    }
+  ]
